c0fb02af39c7794c1a2501efe588ce5d22948594
Haslinda Yahya <haslinda.yahya@icu.gov.my>
2024-10-15 10:41:33 +0800

    1. add based on tutorial

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,53 @@ Route::get('/home', function () {
 Route::get('/signin', function () {
     return view('auth.signin');
 });
+
+// Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'User adalah : '.$name;
+});
+
+// Name Route
+Route::get('/user/profile', function () {
+    return 'User Profile';
+})->name('user.profile');
+
+// alias of a routre user.profile = /pengguna/profile
+
+// Redirect route to named route
+Route::get('/redirect-to-profile',function(){
+    return redirect()->route('user.profile');
+});
+
+// Route Group
+Route::prefix('food')->group(function (){
+
+        Route::get('/details',function (){
+            return 'Food details are following';
+        });
+
+        Route::get('/home',function (){
+            return 'Food home page';
+        });
+});
+
+
+
+//Combination of all (Route, Named Route, Route Param, Route Group. Route Prefix)
+Route::name('job')->prefix('job')->group(function() {
+    
+    Route::get('/home', function () {
+        return 'Job home page';
+    })->name('.name');
+    
+    Route::get('/details', function () {
+        return 'Job details are following';
+    })->name('.description');
+
+});
+
+Route::get('/home/{name}',function ($name){
+        return view('home',['name'=>$name]);
+});
+
+
